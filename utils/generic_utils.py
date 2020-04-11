2e3ba764df7e59a4c50a2d2c1da166ff0b40ec04
erogol <erogol@hotmail.com>
2020-04-11 11:39:54 +0200

    setup model for tacotron2

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -258,17 +258,20 @@ def setup_model(num_chars, num_speakers, c):
                         use_forward_attn_mask=c.forward_attn_mask,
                         use_loc_attn=c.location_attn,
                         separate_stopnet=c.separate_stopnet)
-    # elif c.model.lower() == "tacotron2":
-    #     model = MyModel(num_chars=num_chars,
-    #                     num_speakers=num_speakers,
-    #                     r=c.r,
-    #                     attn_win=c.windowing,
-    #                     attn_norm=c.attention_norm,
-    #                     prenet_type=c.prenet_type,
-    #                     prenet_dropout=c.prenet_dropout,
-    #                     forward_attn=c.use_forward_attn,
-    #                     trans_agent=c.transition_agent,
-    #                     forward_attn_mask=c.forward_attn_mask,
-    #                     location_attn=c.location_attn,
-    #                     separate_stopnet=c.separate_stopnet)
+    elif c.model.lower() == "tacotron2":
+        model = MyModel(num_chars=num_chars,
+                        num_speakers=num_speakers,
+                        r=c.r,
+                        mel_dim=c.audio['num_mels'],
+                        memory_size=c.memory_size,
+                        attn_win=c.windowing,
+                        gst=c.use_gst,
+                        attn_norm=c.attention_norm,
+                        prenet_type=c.prenet_type,
+                        prenet_dropout=c.prenet_dropout,
+                        use_forward_attn=c.use_forward_attn,
+                        use_trans_agent=c.transition_agent,
+                        use_forward_attn_mask=c.forward_attn_mask,
+                        use_loc_attn=c.location_attn,
+                        separate_stopnet=c.separate_stopnet)
     return model
