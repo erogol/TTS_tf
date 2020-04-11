0b139799db387da9f944d80589044557cce6f6d5
erogol <erogol@hotmail.com>
2020-04-11 11:35:37 +0200

    simplify losses using keras

--- layers/losses.py
+++ layers/losses.py
@@ -1,24 +1,27 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.python.ops import math_ops
 
 def loss_l1(y_hat, y, lens):
-    mask = tf.sequence_mask(lens, maxlen=y.shape[1], dtype=tf.float32)
-    mask = tf.cast(mask, tf.float32)
-    loss = tf.abs(y_hat - y)
-    mask = tf.tile(tf.expand_dims(mask, axis=-1), [1, 1, loss.shape[2]])
-    loss_masked = loss * mask
-    return tf.reduce_sum(loss_masked) / tf.reduce_sum(mask)
-
+    #mask = tf.sequence_mask(lens, maxlen=y.shape[1], dtype=tf.float32)
+    #mask = tf.cast(mask, tf.float32)
+    loss = tf.reduce_mean(math_ops.abs(y_hat - y))
+    #mask = tf.tile(tf.expand_dims(mask, axis=-1), [1, 1, loss.shape[2]])
+    #loss_masked = loss * mask
+    #return tf.reduce_sum(loss_masked) / tf.reduce_sum(mask)
+    return loss
 
 def loss_l2(y_hat, y, lens):
-    mask = tf.sequence_mask(lens, dtype=tf.float32)
-    loss = tf.nn.l2_loss(y_hat, y)
-    mask = tf.tile(tf.expand_dims(mask, axis=-1), [1, 1, loss.shape[2]])
-    loss_masked = loss * mask
-    return tf.reduce_sum(loss_masked) / tf.reduce_sum(mask)
+    # mask = tf.sequence_mask(lens, dtype=tf.float32)
+    # loss = tf.keras.losses.mean_squared_error(y, y_hat)
+    loss = tf.reduce_mean(math_ops.squared_difference(y_hat, y))
+    #mask = tf.tile(tf.expand_dims(mask, axis=-1), [1, 1, loss.shape[2]])
+    #loss_masked = loss * mask
+    #return tf.reduce_sum(loss_masked) / tf.reduce_sum(mask)
+    return loss
 
 def stopnet_loss(y_hat, y):
     return tf.reduce_mean(            
-        keras.losses.binary_crossentropy(tf.cast(y, tf.float32),
+        keras.losses.binary_crossentropy(y,
                                          y_hat,
                                          from_logits=True))
